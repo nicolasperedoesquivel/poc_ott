af7c13ecc7b1cd917b8a314e4fab5fb98b80befe
Nicolas Peredo <nicolas.peredo@globant.com>
2022-03-16 17:35:41 -0300

    Adding export of icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './containers/Home';
 import './assets/styles/main.scss'
 
+export const images = require.context('./assets/icons', true);
+
 function App() {
   return (
     <div className="App">
