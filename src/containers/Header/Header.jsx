9b020c85cb9d12562144b96cf48c6de00ca7bab8
Nicolas Peredo <nicolas.peredo@globant.com>
2022-03-16 17:36:08 -0300

    Adding Header component

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,46 @@
+import {useState} from 'react';
+import {images} from '../../App';
+import Button from '../../components/Button/Button';
+import ImageContainer from '../../components/ImageContainer/ImageContainer';
+import InputText from '../../components/InputText/InputText';
+
+
 function Header(){
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleInputChange = (e) => {
+        setSearchValue(e.target.value);
+    }
     return(
-        <div className="header-container">
-            Simple header
-        </div>
+        <header className="header-container">
+            <nav className="header-container__nav-container">
+                <div className="app-logo">
+                    <ImageContainer
+                        imgSrc={images('./camera-film-icon.svg')}
+                        altName="camera-film"
+                        className="app-logo__img"
+                    />
+                </div>
+                <div className="search-container">
+                <InputText
+                    type="text"
+                    name="search-input"
+                    value={searchValue}
+                    className="search-container__input"
+                    onChange={handleInputChange}
+                />
+                <Button
+                    className="search-container__button"
+                    childrenComponent={<ImageContainer
+                        imgSrc={images('./search-icon.svg')}
+                        altName="search-icon"
+                        className="search-container__button__img"
+                    />}
+                />
+                
+                </div>
+            </nav>
+        </header>
     );
 }
 
